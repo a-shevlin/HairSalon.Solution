df9befc508ecb55d65deaad57215b3458c1ed4b4
Alex Shevlin <alexshevlin1@gmail.com>
2022-07-29 11:36:54 -0700

    add HairSalon.csproj and update StylistsController.cs

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -1,13 +1,40 @@
+using System.Collections.Generic;
+using System;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using HairSalon.Models;
 
 namespace HairSalon.Controllers
 {
-  public class HomeController : Controller
+  public class StylistsController : Controller
   {
-    [HttpGet("/")]
+    private readonly HairSalonContext _db;
+
+    public StylistsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
     public ActionResult Index()
     {
+      List<Stylist> model = _db.Stylists.ToList();
+      ViewBag.PageTitle = "View Stylists";
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.PageTitle = "New Stylist";
       return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Stylist stylist)
+    {
+      _db.Stylists.Add(stylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
